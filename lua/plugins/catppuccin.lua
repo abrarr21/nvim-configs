50729d160ab5f640ebbba80991775d028acb9186
abrarr21 <abrarrr2110@gmail.com>
2024-04-20 09:43:14 +0530

    header

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -9,3 +9,12 @@ return {
         vim.cmd.colorscheme "catppuccin"
     end
 }
+
+-- return {
+--     "Tsuzat/NeoSolarized.nvim",
+--       lazy = false, -- make sure we load this during startup if it is your main colorscheme
+--       priority = 1000, -- make sure to load this before all the other start plugins
+--       config = function()
+--         vim.cmd [[ colorscheme NeoSolarized ]]
+--       end
+--   }
