8f23cae9bf3d247e9a5e918a5b5ec55e8a36654a
abrarr21 <abrarrr2110@gmail.com>
2024-09-10 21:12:35 +0530

    toggleTerm/configs

---
+++ lua/plugins/toggleTerm.lua
@@ -0,0 +1,18 @@
+return {
+    {
+    {'akinsho/toggleterm.nvim', version = "*", opts = {
+        open_mapping = [[<c-\>]], -- Mapping to open/close the terminal
+        shade_filetypes = {},
+        shade_terminals = true,
+        shading_factor = 2,
+        start_in_insert = true,
+        insert_mappings = true,
+        terminal_mappings = true,
+        persist_size = true,
+        direction = 'horizontal', -- 'vertical', 'horizontal', 'tab', or 'float'
+    }}
+    },
+
+
+    
+}
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -67,4 +67,7 @@ vim.keymap.set('i', '<C-j>', '<Down>', { noremap = true, silent = true })
 vim.keymap.set('i', '<C-k>', '<Up>', { noremap = true, silent = true })
 vim.keymap.set('i', '<C-l>', '<Right>', { noremap = true, silent = true })
 
+-- Save files with ctrl+s
+vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', { noremap = true, silent = true })
+
 
