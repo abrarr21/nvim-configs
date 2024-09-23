9390e72798a32537779e701d75c12d996b8c8916
abrarr21 <abrarrr2110@gmail.com>
2024-09-23 16:18:37 +0530

    terminal && mini settings

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -27,7 +27,9 @@ return {
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     config = function()
-        vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+        -- vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
         vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+        vim.keymap.set('n', '<C-n>', ':Neotree toggle filesystem left<CR>', { desc = "Toggle Neotree filesystem" })
+
     end
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -17,8 +17,8 @@ opt.background = "dark"
 opt.clipboard:append("unnamedplus")
 
 --split windows
-opt.splitright = true  --split vertical window to the right 
-opt.splitbelow = true  --split horizontal window to the bottom
+opt.splitright = true --split vertical window to the right
+opt.splitbelow = true --split horizontal window to the bottom
 
 opt.tabstop = 2
 opt.shiftwidth = 2
@@ -32,13 +32,12 @@ vim.g.background = "light"
 
 vim.wo.number = true
 
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')   --in normal mode to move to the pane above.
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')   --in normal mode to move to the pane below.
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')   --in normal mode to move to the pane to the left.
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')   --in normal mode to move to the pane to the right.
+vim.keymap.set("n", "<c-k>", ":wincmd k<CR>") --in normal mode to move to the pane above.
+vim.keymap.set("n", "<c-j>", ":wincmd j<CR>") --in normal mode to move to the pane below.
+vim.keymap.set("n", "<c-h>", ":wincmd h<CR>") --in normal mode to move to the pane to the left.
+vim.keymap.set("n", "<c-l>", ":wincmd l<CR>") --in normal mode to move to the pane to the right.
 
-
-vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>")
 
 vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
@@ -47,27 +46,25 @@ vim.keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- incr
 vim.keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
-vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })                   -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })                 -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })                    -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })               -- close current split window
 
-vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })                     -- open new tab
+vim.keymap.set("n", "<leader>tq", "<cmd>tabclose<CR>", { desc = "Close current tab" })              -- close current tab
+vim.keymap.set("n", "L", "<cmd>tabn<CR>", { desc = "Go to next tab" })                              --  go to next tab
+vim.keymap.set("n", "H", "<cmd>tabp<CR>", { desc = "Go to previous tab" })                          --  go to previous tab
+vim.keymap.set("n", "<leader>tt", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 -- Ctrl+o for entering new line in insert mode
-vim.keymap.set('i', '<C-o>', '<Esc>o', { noremap = true, silent = true })
+vim.keymap.set("i", "<C-o>", "<Esc>o", { noremap = true, silent = true })
 
 --Cursor movement while in Insert mode
-vim.keymap.set('i', '<C-h>', '<Left>', { noremap = true, silent = true })
-vim.keymap.set('i', '<C-j>', '<Down>', { noremap = true, silent = true })
-vim.keymap.set('i', '<C-k>', '<Up>', { noremap = true, silent = true })
-vim.keymap.set('i', '<C-l>', '<Right>', { noremap = true, silent = true })
+vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-j>", "<Down>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-k>", "<Up>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true, silent = true })
 
 -- Save files with ctrl+s
-vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', { noremap = true, silent = true })
-
-
+vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", { noremap = true, silent = true })
