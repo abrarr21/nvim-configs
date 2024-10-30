dc8faeea793cafc6b6ecd8cb2fa971d7d36f78c8
abrarr21 <abrarrr2110@gmail.com>
2024-10-30 13:07:03 +0530

    Lazygit added

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -46,15 +46,15 @@ vim.keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- incr
 vim.keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
-vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })                   -- split window vertically
-vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })                 -- split window horizontally
-vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })                    -- make split windows equal width & height
-vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })               -- close current split window
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
-vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })                     -- open new tab
-vim.keymap.set("n", "<leader>tq", "<cmd>tabclose<CR>", { desc = "Close current tab" })              -- close current tab
-vim.keymap.set("n", "L", "<cmd>tabn<CR>", { desc = "Go to next tab" })                              --  go to next tab
-vim.keymap.set("n", "H", "<cmd>tabp<CR>", { desc = "Go to previous tab" })                          --  go to previous tab
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+vim.keymap.set("n", "<leader>tq", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+vim.keymap.set("n", "L", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+vim.keymap.set("n", "H", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 vim.keymap.set("n", "<leader>tt", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 -- Ctrl+o for entering new line in insert mode
@@ -68,3 +68,17 @@ vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true, silent = true })
 
 -- Save files with ctrl+s
 vim.keymap.set("n", "<C-s>", "<cmd> w <CR>", { noremap = true, silent = true })
+
+-- Vertical Scroll and center
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { noremap = true, silent = true })
+
+-- Delete single characters without copying into register
+vim.keymap.set("n", "x", '"_x', { noremap = true, silent = true })
+
+-- Move a line up and down
+vim.keymap.set("n", "<A-k>", ":move -2<CR>==", { desc = "Move line up" })
+vim.keymap.set("n", "<A-j>", ":move +1<CR>==", { desc = "Move line down" })
+
+-- Clear the clipboard
+vim.api.nvim_set_keymap("n", "<leader>cb", ":let @+ = ''<CR>", { noremap = true, silent = true })
